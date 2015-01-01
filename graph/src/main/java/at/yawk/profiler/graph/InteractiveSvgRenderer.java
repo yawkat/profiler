8364f0c193d44789599588e352c58da3d975ccec
Jonas Konrad <me@yawk.at>
2015-01-01 18:52:44 +0100

    Use XSLT for inserting svgpan

--- graph/src/main/java/at/yawk/profiler/graph/InteractiveSvgRenderer.java
+++ graph/src/main/java/at/yawk/profiler/graph/InteractiveSvgRenderer.java
@@ -1,22 +1,19 @@
 package at.yawk.profiler.graph;
 
 import at.yawk.profiler.sampler.StackGraph;
+import com.google.common.io.ByteStreams;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.batik.dom.util.SAXDocumentFactory;
-import org.apache.batik.util.XMLResourceDescriptor;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 
 /**
  * @author yawkat
@@ -24,11 +21,22 @@ import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 @Slf4j
 @RequiredArgsConstructor
 public class InteractiveSvgRenderer implements GraphRenderer {
+    private static final String TRANSFORM_XSLT;
+    private static final String DEFAULT_SVGPAN_URI = "https://www.cyberz.org/projects/SVGPan/SVGPan.js";
+
     private final GraphRenderer renderer;
     private final String svgPan;
 
+    static {
+        try (InputStream is = InteractiveSvgRenderer.class.getResourceAsStream("svgpan.xslt")) {
+            TRANSFORM_XSLT = new String(ByteStreams.toByteArray(is), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public InteractiveSvgRenderer(GraphRenderer renderer) {
-        this(renderer, "https://www.cyberz.org/projects/SVGPan/SVGPan.js");
+        this(renderer, DEFAULT_SVGPAN_URI);
     }
 
     @Override
@@ -37,51 +45,16 @@ public class InteractiveSvgRenderer implements GraphRenderer {
 
         log.debug("Adding SVGPan to SVG output ({} characters)", svg.length());
 
-        Document doc;
         try {
-            String parserName = XMLResourceDescriptor.getXMLParserClassName();
-            SAXDocumentFactory factory = new SAXDocumentFactory(
-                    DOMImplementationRegistry.newInstance().getDOMImplementation("XML 1.0"),
-                    parserName
-            );
-            doc = factory.createDocument(null, new StringReader(svg));
-        } catch (IOException | ReflectiveOperationException e) {
-            throw new RuntimeException(e);
-        }
-
-        Element root = doc.getDocumentElement();
-        root.removeAttribute("viewBox");
-
-        Element oldViewport = null;
-
-        for (int i = 0; i < root.getChildNodes().getLength(); i++) {
-            Node node = root.getChildNodes().item(i);
-            if (node.getNodeName().equalsIgnoreCase("g")) {
-                oldViewport = (Element) node;
-                break;
-            }
-        }
-
-        if (oldViewport == null) {
-            throw new UnsupportedOperationException("Missing g element");
-        }
-
-        Element newViewport = doc.createElement("g");
-        newViewport.setAttribute("id", "viewport");
-
-        root.replaceChild(newViewport, oldViewport);
-        newViewport.appendChild(oldViewport);
-
-        Element script = doc.createElement("script");
-        script.setAttribute("xlink:href", svgPan);
-        root.insertBefore(script, root.getFirstChild());
-
-        try {
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            StreamSource xslt = new StreamSource(new StringReader(TRANSFORM_XSLT));
+            Transformer transformer = TransformerFactory.newInstance().newTransformer(xslt);
+            transformer.setParameter("svgpan-uri", svgPan);
 
             StringWriter sw = new StringWriter();
-            StreamResult result = new StreamResult(sw);
-            transformer.transform(new DOMSource(doc), result);
+            transformer.transform(
+                    new StreamSource(new StringReader(svg)),
+                    new StreamResult(sw)
+            );
 
             return sw.toString();
         } catch (TransformerException e) {
---
+++ graph/src/main/resources/at/yawk/profiler/graph/svgpan.xslt
@@ -0,0 +1,28 @@
+<?xml version="1.0"?>
+
+<xsl:stylesheet version="1.0"
+                xmlns:svg="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:param name="svgpan-uri"/>
+
+    <xsl:output method="xml" indent="yes"/>
+
+    <xsl:template match="node()|@*">
+        <xsl:copy>
+            <xsl:apply-templates select="node()|@*"/>
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="/svg:svg/svg:g">
+        <script xlink:href="{$svgpan-uri}"/>
+        <g id="viewport">
+            <xsl:copy>
+                <xsl:apply-templates select="node()|@*"/>
+            </xsl:copy>
+        </g>
+    </xsl:template>
+
+    <xsl:template match="/svg:svg/@viewBox">
+    </xsl:template>
+</xsl:stylesheet>

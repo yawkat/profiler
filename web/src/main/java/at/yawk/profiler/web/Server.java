a643d4ef1d21eb36862bafd5d1129f37625cd1cb
Jonas Konrad <me@yawk.at>
2015-01-02 16:34:26 +0100

    Error page

--- web/src/main/java/at/yawk/profiler/web/Server.java
+++ web/src/main/java/at/yawk/profiler/web/Server.java
@@ -1,6 +1,10 @@
 package at.yawk.profiler.web;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
 import fi.iki.elonen.NanoHTTPD;
+import java.io.IOException;
+import java.util.Map;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 class Server extends NanoHTTPD {
+    private static final Gson GSON = new Gson();
+
     @Setter private Handler rootHandler = new ContextHandler("");
 
     public Server(String hostname, int port) {
@@ -21,15 +27,37 @@ class Server extends NanoHTTPD {
 
         HttpSession wrapper = new HttpSession(session);
 
+        boolean printError = false;
+
+        String path = session.getUri();
+        if (path.endsWith("/")) { path += "index.html"; }
         try {
-            String path = session.getUri();
-            if (path.endsWith("/")) { path += "index.html"; }
             if (!rootHandler.serve(wrapper, path)) {
                 wrapper.response.setStatus(Response.Status.NOT_FOUND);
+                printError = true;
             }
         } catch (Throwable t) {
             log.warn("Error while serving request to " + session.getUri(), t);
             wrapper.response.setStatus(Response.Status.INTERNAL_ERROR);
+            printError = true;
+        }
+
+        if (printError) {
+            Response.IStatus status = wrapper.response.getStatus();
+            Map<String, Object> statusMap = ImmutableMap.of(
+                    "code", status.getRequestStatus(),
+                    "message", status.getDescription().substring(status.getDescription().indexOf(' ') + 1)
+            );
+            if (path.endsWith(".html")) {
+                try {
+                    wrapper.data("text/html",
+                                 TemplateManager.getInstance().compile("error", statusMap));
+                } catch (IOException e) {
+                    log.warn("Error while serving error page to " + session.getUri());
+                }
+            } else {
+                wrapper.data("application/json", GSON.toJson(statusMap));
+            }
         }
 
         return wrapper.response;
